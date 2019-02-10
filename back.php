07b8b1a4407b2525f0041fe1aa61add00a6db7c6
deoroop <47508254+deoroop@users.noreply.github.com>
2019-02-10 16:58:28 -0500

    Create back.php

---
+++ back.php
@@ -0,0 +1,31 @@
+<?php
+    $con = mysql_connect("sql.njit.edu","Accounts");
+    if(!$con) {
+        die('Could not connect: ' . mysql_error());
+    }
+    mysql_select_db("Accounts", $con);
+   
+    $User = $_POST['Username'];
+    $Pass = $_POST['Password'];
+    $check = $_POST['Check'];
+    
+  
+    
+   if ($check == 'student') {
+        
+        $sql = mysql_query("SELECT COUNT(*) FROM StudentLogin SL WHERE Student_ID = '$User' AND Student_PW = '$Pass'");
+        $info = mysql_fetch_assoc($sql);
+        $ver = $info['COUNT(*)'];
+        
+        if ($ver == 1) {
+            session_start();
+            $_SESSION['UserST'] = $User;
+            echo "student connected";
+        }
+        else {
+            header ("Location: http://web.njit.edu/~sd744/CS490/Front/login.html");
+            echo "Invalid Login";
+        } 
+    } 
+    
+?>
