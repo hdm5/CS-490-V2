9cddd8b0824de7046e964ca181825f167383d436
hdm5 <47505698+hdm5@users.noreply.github.com>
2019-02-10 22:16:21 -0500

    Update action_page.php

--- action_page.php
+++ action_page.php
@@ -1,27 +1,31 @@
+
 <?php
 $username = $_POST['username'];
 $password = $_POST['password'];
 
-#$url= "https://postman-echo.com/post";
-
-$url = "https://web.njit.edu/~pm347/doLogin.php";
+$url = "https://web.njit.edu/~pm347/middleServer.php";
 
 $ch = curl_init( $url );
-
-# Setup request to send json via POST.
 $payload = json_encode( array( "username"=> $username, "password"=>$password) );
 curl_setopt( $ch, CURLOPT_POSTFIELDS, $payload );
 curl_setopt( $ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
-
-# Return response instead of printing.
 curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
-
-# Send request.
 $result = curl_exec($ch);
+$httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 curl_close($ch);
 
-# Print response.
-echo "Result = $result";
-$httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-echo "http CODE = $httpcode";
+$data = json_decode($result,true);
+if (!isset($data['authenticated'])) {
+        echo "User not found, authentication failed!";
+}
+else{
+        $authenticate = $data["authenticated"];
+        if($authenticate === true){
+                echo "Authentication success!";
+        }
+        else{
+                echo "Authentication failed!";
+        }
+}
+
 ?>
