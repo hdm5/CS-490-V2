8e4ba7671b80ca8cd2a7f6aad8fcf5b2962ac6ab
hdm5 <47505698+hdm5@users.noreply.github.com>
2019-02-18 04:17:35 -0500

    Update middleServer.php

--- middleServer.php
+++ middleServer.php
@@ -1,24 +1,91 @@
 <?php
+  header("Access-Control-Allow-Origin: *");
+  
+  # Receive request JSON from front-end
+  $frontend_data = file_get_contents('php://input');
+  $logindata = json_decode($frontend_data, true);
 
-# Receive request JSON
-$frontendData = file_get_contents('php://input');
-$backendUrl = "https://web.njit.edu/~sd744/back.php";
+  $username = "";
+  $password = "";	
+  if(isset($logindata['username']))
+    $username = $logindata['username'];
+  if(isset($logindata['password']))
+    $password = $logindata['password'];
+ 
+  # fall back to backend database if login with njit fails
+  if (check_login_njit($username,$password) == true) {
+    $authenticated = true;
+    $studentNJIT = "njit";
+  }
+  else if(check_login_backend_db($username,$password) == true) {
+    $authenticated = true;
+    $studentNJIT = "database";
+  }
+  else {
+    $authenticated = false;
+    $studentNJIT = "";
+  }
 
-# Forward request from frontend to backend
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $backendUrl);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $frontendData);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-$curlResult = curl_exec($ch);
-$returnCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-curl_close($ch);
+  # return response to frontend
+  die (json_encode(array("authenticated" => $authenticated, "studentNJIT" => $studentNJIT)));
+  
+  function check_login_njit($username, $password)
+  {
+    # make a json object of username and password                
+    $payload = array(
+    "ucid" => $username,
+    "pass" => $password);
+    
+    $njitUrl = "https://aevitepr2.njit.edu/myhousing/login.cfm";
+    $chsession = curl_init($njitUrl);
+    curl_setopt($chsession, CURLOPT_POST, true);
+    curl_setopt($chsession, CURLOPT_POSTFIELDS , http_build_query($payload));
+    curl_setopt($chsession, CURLOPT_SSL_VERIFYHOST, false);
+    curl_setopt($chsession, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($chsession, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36 Edge/17.17134');
+    curl_setopt($chsession, CURLOPT_FOLLOWLOCATION, true);
+    curl_setopt($chsession, CURLOPT_RETURNTRANSFER, true);
+    
+    $response = curl_exec($chsession);
+    $returnCode = curl_getinfo($chsession, CURLINFO_HTTP_CODE);
+    
+    curl_close($chsession);		
+    if ($returnCode === 200)
+      http_response_code(200);
+    else
+      http_response_code(401);
+    
+    $returnMsg = "";
+    if(strpos($response, 'Please select a MyHousing System to sign into') == true)			            
+      return true;
+    else
+      return false;
+  }
+  
+  function check_login_backend_db($username,$password)
+  {
+    # make a json object of username and password        
+    $payload = json_encode(array("username"=>$username, "password"=>$password));
+    $backendUrl = "https://web.njit.edu/~sd744/back.php";
 
-if ($returnCode === 200)
-    http_response_code(200);
-else
-    http_response_code(401);
+    $ch = curl_init($backendUrl);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
+    $response = curl_exec($ch);
+    $returnCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    curl_close($ch);
+    
+    if ($returnCode === 200)
+        http_response_code(200);
+    else
+        http_response_code(401);
 
-# Return the backend response to frontend
-die ($curlResult);
-
+    $data = json_decode($response, true);
+    if (!isset($data['authenticated']))
+        return false;
+    else
+        return $data["authenticated"];
+  }
 ?>
