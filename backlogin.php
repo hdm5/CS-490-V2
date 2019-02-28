f857b4a31652c004af30a5c9c205a1930f1a23e9
deoroop <47508254+deoroop@users.noreply.github.com>
2019-02-28 17:46:52 -0500

    Create backlogin.php

---
+++ backlogin.php
@@ -0,0 +1,47 @@
+<?php
+    
+    $con = mysqli_connect("sql1.njit.edu","sd744","DVDjpHtNw", "sd744");
+    if(!$con) {
+        die('Could not connect: ' . mysql_error());
+    }
+  
+   $data=json_decode(file_get_contents('php://input'),true); 
+   $uname = $data['username'];
+   $pass = $data['password'];
+   $check = $data['check']
+   
+   if ($check == 'jsn45') {
+	
+   $sql = mysqli_query($con, "SELECT COUNT(*) FROM Accounts WHERE Username = '$uname' AND Password = '$pass'");
+   $info = mysqli_fetch_assoc($sql);
+   $count = $info['COUNT(*)'];
+  
+   $authenticated;
+   if ($count == 1) {
+        $authenticated = $uname;
+        echo "teacher is connected";
+	http_response_code(200);
+   }
+   else {
+        echo "Invalid Login";
+	http_response_code(401);
+   }
+}
+   if ($check == 'jsn1'){
+   $sql = mysqli_query($con, "SELECT COUNT(*) FROM Accounts WHERE Username = '$uname' AND Password = '$pass'");
+   $info = mysqli_fetch_assoc($sql);
+   $count = $info['COUNT(*)'];
+  
+   $authenticated;
+   if ($count == 1) {
+        $authenticated = $uname;
+        echo "student is connected";
+	http_response_code(200);
+   }
+   else {
+        echo "Invalid Login";
+	http_response_code(401);
+   }
+   }
+    die (json_encode(array("authenticated" => $authenticated)));
+?>
