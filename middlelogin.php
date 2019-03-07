29a6c598d3a32c5e0ee73e547ca7fd1d6066ab3b
avinashvaddadi <47508617+avinashvaddadi@users.noreply.github.com>
2019-03-06 22:32:26 -0500

    Add files via upload

--- middlelogin.php
+++ middlelogin.php
@@ -1,39 +1,25 @@
 <?php
+
   header("Access-Control-Allow-Origin: *");
   
   # Receive request JSON from front-end
   $frontend_data = file_get_contents('php://input');
   $logindata = json_decode($frontend_data, true);
   
-  $username = "";
-  $password = "";
-  $check = "";
-  if(isset($logindata['username']))
-    $username = $logindata['username'];
-  if(isset($logindata['password']))
-    $password = $logindata['password'];
-   if(isset($logindata['check']))
-    $check = $logindata['check'];
+  $Username = "";
+  $Password = "";
+  $Check = "";
+
+  if(isset($logindata['Username']))
+    $Username = $logindata['Username'];
+  if(isset($logindata['Password']))
+    $Password = $logindata['Password'];
+  if(isset($logindata['Check']))
+    $Check = $logindata['Check'];
  
-  
  
-  if(check_login_backend_db($username,$password, $check) == true) {
-    $authenticated = true;
-    $studentNJIT = "Database";
-  }
-  else {
-    $authenticated = false;
-    $studentNJIT = "";
-  }
-  # return response to frontend
-  die (json_encode(array("authenticated" => $authenticated, "studentNJIT" => $studentNJIT)));
-  
- 
-  
-  function check_login_backend_db($username,$password,$check)
-  {
     # make a json object of username, password and check        
-    $payload = json_encode(array("username"=>$username, "password"=>$password,"check"=>$check));
+    $payload = json_encode(array("Username"=>$Username, "Password"=>$Password,"Check"=>$Check));
     $backendUrl = "https://web.njit.edu/~sd744/backlogin.php";
     $ch = curl_init($backendUrl);
     curl_setopt($ch, CURLOPT_POST, true);
@@ -48,10 +34,13 @@
         http_response_code(200);
     else
         http_response_code(401);
+
     $data = json_decode($response, true);
-    if (!isset($data['authenticated']))
-        return false;
-    else
-        return $data["authenticated"];
-  }
+
+    $authenticated = $data['authenticated'];
+  
+
+    # return response to frontend
+    die (json_encode(array("authenticated" => $authenticated)));
+  
 ?>
