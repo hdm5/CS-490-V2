4392fd77c1e0592530d5f6a4356ca05c01797c92
hdm5 <47505698+hdm5@users.noreply.github.com>
2019-04-15 01:01:03 -0400

    Create UPDATEDlogin.php

---
+++ UPDATEDlogin.php
@@ -0,0 +1,45 @@
+<?php
+	session_set_cookie_params(0, "/~bg252/");
+	session_start();
+	$return_arr["uname"] = $_POST["uname"];
+	$return_arr["psw"] = $_POST["psw"];
+	$url = 'https://web.njit.edu/~bg252/download/CS490/Beta/login.php'; /* URL of mid end php file goes in between apostrophes */
+	$ch = curl_init($url);
+	$postString = http_build_query($return_arr, '', '&');
+	curl_setopt($ch, CURLOPT_POST, 1);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $postString);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  
+ 
+	$resp = curl_exec($ch); 
+	curl_close($ch);
+	
+	$js = json_decode($resp);
+	
+	if($js->Valid == 1)
+	{	
+		
+		$_SESSION["logged"] = true;
+		$ucid = $return_arr["uname"];
+		$_SESSION["UCID"] = $ucid;
+		
+		if($js->Type == "student") 
+		{
+			#echo '{"Type":"student"}';
+      $url = 'https://web.njit.edu/~hdm5/RC/student_page.html';
+		  die ('<META HTTP-EQUIV=REFRESH CONTENT="1; '.$url.'">');
+		}
+		
+		else if($js->Type == "teacher")
+		{
+			#echo '{"Type":"teacher"}';
+      $url = 'https://web.njit.edu/~hdm5/RC/instructor_page.html';
+		  die ('<META HTTP-EQUIV=REFRESH CONTENT="1; '.$url.'">');
+		}
+	}
+	else
+	{
+		echo'Authentication failed!';   
+	}
+	
+?>
